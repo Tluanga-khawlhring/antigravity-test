fa5a7c7a687bdb742afc4feaadd584a2532ce594
shadowy <tluangakhawlhring17@gmail.com>
2026-06-06 01:12:45 +0530

    added google api

--- config.js
+++ config.js
@@ -3,5 +3,6 @@
  * Auto-generated — upload this file to Netlify to sync across all devices.
  */
 window.SK_CONFIG = {
-  firebaseUrl: 'https://skkktp-default-rtdb.asia-southeast1.firebasedatabase.app/'
+  firebaseUrl: 'https://skkktp-default-rtdb.asia-southeast1.firebasedatabase.app/',
+  googleDriveApiKey: ''
 };
