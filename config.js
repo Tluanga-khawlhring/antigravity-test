ea0509f0dd21b3d6881a355b3c7efedcfb00f94b
shadowy <tluangakhawlhring17@gmail.com>
2026-06-06 01:18:36 +0530

    update config.js

--- config.js
+++ config.js
@@ -4,5 +4,5 @@
  */
 window.SK_CONFIG = {
   firebaseUrl: 'https://skkktp-default-rtdb.asia-southeast1.firebasedatabase.app/',
-  googleDriveApiKey: ''
+  googleDriveApiKey: 'AIzaSyD_x9uKnhwF_FFwVR6tDhQWlk4rMQNE5vA'
 };
